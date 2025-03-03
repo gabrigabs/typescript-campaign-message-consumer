936b17a53e4629325c133ce465f54f17b8659b41
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-03 15:10:21 -0300

    feat: enhance campaign message consumer service with RabbitMQ integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import mongodb from './db/mongo.db';
+import postgresdb from './db/postgres.db';
+import rabbitmqService from './services/rabbitmq.service';
+import logger from './utils/logger';
 
 async function main(): Promise<void> {
-  await mongodb.connect();
+  try {
+    logger.info('Starting campaign message consumer service...');
+    await mongodb.connect();
+    await postgresdb.connect();
+
+    await rabbitmqService.connect();
+    await rabbitmqService.startConsuming();
+
+    logger.info('Campaign message consumer service started successfully');
+  } catch (error) {
+    logger.error('Error starting service', { error });
+    process.exit(1);
+  }
 }
 
 main();
